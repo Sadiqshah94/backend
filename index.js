3a71e0ca7d69cc8dc7d95175b8f7ffd94a4a4a0f
Sadiqshah94 <153868113+Sadiqshah94@users.noreply.github.com>
2024-11-29 17:52:52 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
+import cors from 'cors';
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes/productRoutes.js';
 
 const app = express();
 dotenv.config();
+
+// Enable CORS
+app.use(cors()); // Add this line
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    console.log("Welcome to express")
-    res.status(200).send('Welcome to express')
- })
-
+    console.log("Welcome to express");
+    res.status(200).send('Welcome to express');
+});
 
 app.use('/products', router);
 
-
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log("Server Started", PORT);
-})
-
+    console.log("Server Started on PORT:", PORT);
+});
